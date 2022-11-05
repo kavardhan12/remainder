4affb7daf3b01cb179e7a28d6bdd0692b70bfff1
Ananda Vardhan <113274852+kavardhan12@users.noreply.github.com>
2022-11-05 12:48:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ while True:
    
     
     
-    df = df[(df["Date"].dt.strftime("%d")==current_time.strftime("%d"))&(pd.datetime.now().strftime('%H:%M')=='07:07')]
+    df = df[(df["Date"].dt.strftime("%d")==current_time.strftime("%d"))&(pd.datetime.now().strftime('%H:%M')=='07:25')]
     
     
 
